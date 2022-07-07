8ae9c0b7f64d22dda883fda48a6c656504524503
Anon Souriyong <anon270@revature.net>
2022-07-06 20:19:22 -0500

    add MyFirstDotNet

---
+++ MyFirstDotNet/MyFirstDotNet/Program.cs
@@ -0,0 +1,6 @@
+//Console.Writeline is from the system library, and allows us to write a line of text to>
+//(____)parameters. Sends whatever is inside parentheses to Console.Writeline
+//" " denotes a string literal
+// ; endline character
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
